48cf5f06cb5b8cad1034695774e4614e395e5118
jipson7 <calebrphillips@gmail.com>
2016-01-18 19:50:41 -0500

    Load config settings from environment variable and add route to test

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return 'Hello World!', 200
+
+@app.route('/env')
+def env_test():
+    return str(os.environ['APP_SETTINGS']), 200
+
 
 if __name__=='__main__':
     app.run()
