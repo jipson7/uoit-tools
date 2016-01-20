6f7b122161db5f3b5e8d568e2f5a381cb52238d5
jipson7 <calebrphillips@gmail.com>
2016-01-19 21:19:03 -0500

    Update model to match db

--- uoit_tools/models.py
+++ uoit_tools/models.py
@@ -1,18 +1,5 @@
 from uoit_tools import db
 
-class Course(db.Model):
-    __tablename__ = 'course'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))              # Max found = 57
-    department = db.Column(db.String(4))
-    code = db.Column(db.String(5))
-    section = db.Column(db.String(3))
-    capacity_total = db.Column(db.Integer)
-    capacity_taken = db.Column(db.Integer)
-    capacity_remaining = db.Column(db.Integer)
-    days = db.relationship('Day')
-
-
 class Day(db.Model):
     __tablename__ = 'day'
     id = db.Column(db.Integer, primary_key=True)
@@ -25,5 +12,19 @@ class Day(db.Model):
     end_date = db.Column(db.Date, nullable=True)
     section_type = db.Column(db.String(32)) # Max found = 17
     instructors = db.Column(db.String(256)) # Max found = 202
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    course = db.relationship('Course')
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id', ondelete='CASCADE'))
+
+class Course(db.Model):
+    __tablename__ = 'course'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))              # Max found = 57
+    department = db.Column(db.String(4))
+    code = db.Column(db.String(5))
+    section = db.Column(db.String(3))
+    capacity_total = db.Column(db.Integer)
+    capacity_taken = db.Column(db.Integer)
+    capacity_remaining = db.Column(db.Integer)
+    semester = db.Column(db.String(6))
+    days = db.relationship(Day, backref='course', passive_deletes=True)
+
+
