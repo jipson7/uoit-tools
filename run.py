ccb98c956ffb970e5913addb3e673a9319b2f082
jipson7 <calebrphillips@gmail.com>
2016-01-19 19:36:10 -0500

    Change local environment to use staging db

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 import os
 
 os.environ['APP_SETTINGS']="uoit_tools.config.DevelopmentConfig"
-os.environ['DATABASE_URL']="postgresql://localhost/uoit_schedule_dev"
+os.environ['DATABASE_URL']="postgres://fuxkqlurxyswbe:YlAF78BEc4V1gI4iTPvDW3p08J@ec2-54-225-165-132.compute-1.amazonaws.com:5432/dk2124iurbta4"
 
 from uoit_tools import app
 
