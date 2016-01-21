15b41702995caf6d3d375364119512ee236d7fc6
jipson7 <calebrphillips@gmail.com>
2016-01-20 21:20:23 -0500

    Add Bootstrap template and add some testing routes

--- uoit_tools/views/main.py
+++ uoit_tools/views/main.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from uoit_tools.models import Course, Day
 
 main = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET'])
 def home_get():
-    return 'Hello World from Blueprint', 200
+    return render_template('index.html'), 200
+
+
+@main.route('/dbtest', methods=['GET'])
+def test_get():
+    test_course = Course.query.filter_by(code='3010U').first()
+    print(dir(test_course))
+    return test_course.code, 200
+
