41f73ed54edac6edcf48505d78cda608b2e01fd6
jipson7 <calebrphillips@gmail.com>
2016-01-23 11:46:12 -0500

    Conolidate date and time libraries with bootstrap datetime picker

--- uoit_tools/static/js/roomfinder.js
+++ uoit_tools/static/js/roomfinder.js
@@ -1,12 +1,8 @@
 
 $(function () {
     var today = new Date();
-    $('#roomfinder-date').datepicker({
-        'startDate': today,
-        autoclose: true
+    $('#roomfinder-datetime').datetimepicker({
+        defaultDate: today,
+        stepping: 30
     });
-    $('#roomfinder-date').datepicker('update', today);
-
-    $('#roomfinder-start').timepicker();
-    $('#roomfinder-end').timepicker();
 });
