edbc2dc64fe26ff9f96b13f1d86ff336a34e8e7d
jipson7 <calebrphillips@gmail.com>
2016-01-30 18:08:15 -0500

    Change contact form to angular validation

--- uoit_tools/static/contact/contact.js
+++ uoit_tools/static/contact/contact.js
@@ -1,7 +1,11 @@
 
 angular.module('uoit-tools')
 .controller('contactController', ['$scope', '$http', function($scope, $http) {
-
+    $scope.submitForm = function(isValid) {
+        if (isValid) {
+            console.log('submitting valid form')
+        }
+    }
 }])
 .directive('contact', function() {
     return {
