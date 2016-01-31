398ce5668869affdb41370de48c8b4283d5102e7
jipson7 <calebrphillips@gmail.com>
2016-01-31 09:35:27 -0500

    Add validation to submitted course list and related error handling

--- uoit_tools/static/scheduler/scheduler.js
+++ uoit_tools/static/scheduler/scheduler.js
@@ -25,8 +25,45 @@ angular.module('uoit-tools')
     }
 
     $scope.submitSchedule = function() {
-        console.log($scope.schedule);
+        var courses = $scope.schedule.courses;
+        $scope.courseListError = null;
+        invalidCourses = [];
+        validCourses = []
+        for (var i in courses) {
+            var course = cleanCourse(courses[i]);
+            if (!course) {
+                invalidCourses.push(courses[i]); 
+            } else {
+                validCourses.push(course);
+            }
+        }
+        if (invalidCourses.length) {
+            $scope.courseListError = 'The following course codes are invalid: ' 
+                + invalidCourses.join(', ');
+        }
+        console.log(validCourses);
     };
+
+    function cleanCourse(course) {
+        course = course.replace(/\s/g, '').toUpperCase();
+        var deptRE = /^([A-Z]{3,4})/;
+        var codeRE = /([0-9]{4})/;
+        var dept = deptRE.exec(course);
+        var code = codeRE.exec(course);
+        if (!dept || !code) {
+            return false;
+        } else {
+            dept = dept[0];
+            code = code[0];
+        }
+        var year = parseInt(code.charAt(0));
+        if (year < 5) {
+            code += 'U';
+        } else {
+            code += 'G';
+        }
+        return (dept + code);
+    }
 
     init();
 }])
