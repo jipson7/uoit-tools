c20bb8e6124fd3cbd773fbb042657abce47f54b3
jipson7 <calebrphillips@gmail.com>
2016-02-01 20:06:12 -0500

    Remove test route from main

--- uoit_tools/views/main.py
+++ uoit_tools/views/main.py
@@ -6,10 +6,3 @@ main = Blueprint('main', __name__)
 @main.route('/', methods=['GET'])
 def home_get():
     return render_template('index.html'), 200
-
-@main.route('/testmodel', methods=['GET'])
-def test_model():
-    x = Course.query.first()
-    return 'ok', 200
-
-
