d63539f494b2098179c16c9307027280bd83e88d
jipson7 <calebrphillips@gmail.com>
2016-02-01 20:12:08 -0500

    Add testing route and beginning of scheduling route

--- uoit_tools/views/scheduler.py
+++ uoit_tools/views/scheduler.py
@@ -1,23 +1,40 @@
-import json, datetime
+import json, datetime, copy
 from flask import Blueprint, request
 from sqlalchemy import or_, not_
 from uoit_tools.models import Course, Day
 from dateutil import parser as date_parser
 from datetime import timedelta, datetime
+#Debuggy
+from uoit_tools import db
+from flask import jsonify
 
 scheduler = Blueprint('scheduler', __name__)
+
+
+@scheduler.route('/test', methods=['GET'])
+def test_gres():
+    query = db.session.query(Day.section_type.distinct().label("title"))
+    titles = [row.title for row in query.all()]
+    return jsonify({'result':titles}), 200
+
 
 @scheduler.route('', methods=['POST'])
 def create_schedules():
     f = request.form;
     semester = f.get('semester', None)
-    courses = f.getlist('courses[]', None)
-    print(courses)
-    print(semester)
+    names = f.getlist('courses[]', None)
+
+    for name in names:
+        courses = Course.query.\
+            filter_by(course_code=name, semester=semester).all()
+        types_added = []
+        for course in courses:
+            print(course.type)
     return 'OK', 200
 
 @scheduler.route('/semesters', methods=['GET'])
 def get_available_semesters():
+    #This function needs to be finished with a query to the db.
     semesters = {
         'January (Winter) 2016': '201601',
         'September (Fall) 2015': '201509'
@@ -43,3 +60,16 @@ def get_day_code(d):
     i = d.weekday()
     if i < 5:
         return codes[i]
+
+
+class Schedule:
+
+    M = []
+    T = []
+    W = []
+    R = []
+    F = []
+
+    # Returns true if the add was a success, false otherwise
+    def add_course(self, name, section):
+        course = Course.query.filter_by(course_code=name, section=section).all()
