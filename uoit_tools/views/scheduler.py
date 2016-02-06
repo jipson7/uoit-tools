dcb7112937adec2d18a03e6abffa431f0af54333
jipson7 <calebrphillips@gmail.com>
2016-02-05 22:01:20 -0500

    Begin adding scheduler logic... too tired to finish tonight

--- uoit_tools/views/scheduler.py
+++ uoit_tools/views/scheduler.py
@@ -13,9 +13,13 @@ scheduler = Blueprint('scheduler', __name__)
 
 @scheduler.route('/test', methods=['GET'])
 def test_gres():
-    query = db.session.query(Day.section_type.distinct().label("title"))
-    titles = [row.title for row in query.all()]
-    return jsonify({'result':titles}), 200
+    course = Course.query.filter_by(course_code='CSCI2020U').first()
+    schedule = Schedule()
+    schedule.add(course.days)
+    return 'ok', 200
+    #query = db.session.query(Day.section_type.distinct().label("title"))
+    #titles = [row.title for row in query.all()]
+    #return jsonify({'result':titles}), 200
 
 
 @scheduler.route('', methods=['POST'])
@@ -24,12 +28,25 @@ def create_schedules():
     semester = f.get('semester', None)
     names = f.getlist('courses[]', None)
 
+    schedules = [Schedule()]
+
+    def expandSchedules(course, days):
+        for schedule in schedules:
+            if schedule.contains(course):
+                schedules.append(schedule.copy_and_delete(course))
+                continue
+            if not schedule.add(days):
+                schedules.remove(schedule)
+
     for name in names:
-        courses = Course.query.\
-            filter_by(course_code=name, semester=semester).all()
-        types_added = []
-        for course in courses:
-            print(course.type)
+        sections = Course.query.filter_by(course_code=name, semester=semester).all()
+        for course in sections:
+            if course.type == 'Lecture':
+                expandSchedules(course, course.days)
+            else:
+                for day in course.days:
+                    expandSchedules(course, [day])
+
     return 'OK', 200
 
 @scheduler.route('/semesters', methods=['GET'])
@@ -55,21 +72,33 @@ def get_semester_code(d=datetime.now()):
         m = '09'
     return str(d.year) + m
 
-def get_day_code(d):
-    codes = ['M', 'T', 'W', 'R', 'F']
-    i = d.weekday()
-    if i < 5:
-        return codes[i]
-
 
 class Schedule:
 
-    M = []
-    T = []
-    W = []
-    R = []
-    F = []
+    weekdays = {
+        'M': [],
+        'T': [],
+        'W': [],
+        'R': [],
+        'F': []
+    }
 
     # Returns true if the add was a success, false otherwise
-    def add_course(self, name, section):
-        course = Course.query.filter_by(course_code=name, section=section).all()
+    def add(self, days):
+        for day in days:
+            slot = {
+                'name': day.course.course_code,
+                'start': day.start_time,
+                'end': day.end_time,
+                'type': day.section_type
+            }
+            self.weekdays[day.day].append(slot)
+        print(self.weekdays)
+
+    # Check if the specific course type exists in schedule (i.e. lab or whatever)
+    def contains(self, course):
+        return False
+
+    def copy_and_delete(self, course):
+        #return a copy of the entire schedule with the specified course type deleted
+        pass
